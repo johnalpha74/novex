a9088d79add2a4b6bd6b44e3386f6ad8d95edfc6
johnalpha74 <johnalpha@gmail.com>
2024-09-01 18:11:01 +0200

    CHORE: First commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 
+ * Main template file
+ * 
+ * @package Novex
+ */
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ 
+ <head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="style.css">
+ <title>Novex Theme</title>
+ </head>
+ 
+ <body>
+ 
+ <main>
+ <H1>Hello Novex</H1>
+ </main>
+ 
+ <script src="script.js"></script>
+ </body>
+ 
+ </html>
