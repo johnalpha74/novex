ef6ec4573f976f5de9635110a24010427e565789
johnalpha74 <johnalpha@gmail.com>
2024-09-01 19:39:35 +0200

    CHORE: Add functions namely get_header.php, language_attributes, wp_body_open, body_class

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ * Header template
+ * 
+ * @package Novex
+ * 
+ */
+?>
+
+<!DOCTYPE html>
+    <html lang="<?php language_attributes(); ?>">
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Novex Theme</title>
+        <?php wp_head(); ?>
+    </head>
+  <body>
+    <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+     <!-- Body content -->
+<header>header</header>
